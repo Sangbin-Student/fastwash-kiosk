fa74e945fb45b5de804f2dfdf1c49c6ae3f5af70
ericlee05 <develretr0@gmail.com>
2023-06-28 11:55:34 +0900

    back-up with font

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         useMaterial3: true,
+        fontFamily: 'NotoSansCJK'
       ),
       home: const MyHomePage(),
     );
